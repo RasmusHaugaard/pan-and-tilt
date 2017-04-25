f4df6a9256abdb375cde36d0c9dc732164962738
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-04-25 15:44:09 +0200

    added hall sensor fpga register addresses

--- mic/pan_tilt_config.h
+++ mic/pan_tilt_config.h
@@ -2,6 +2,10 @@
 #define PAN_TILT_CONFIG_H_
 #include "emp_type.h"
 
+#define FPGA_REG_HOMING_PAN 5
+#define FPGA_REG_HOMING_TILT 6
+#define DUMMY 0
+
 FP32 ticks_to_rad(INT16S);
 INT8S voltage_to_dutycycle(FP32);
 
