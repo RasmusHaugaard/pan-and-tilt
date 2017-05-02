f415259259b283f99e8aa87e8cdca226e96bd35e
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2017-05-02 13:10:56 +0200

    Accelerometer test

--- mic/accelerometer.c
+++ mic/accelerometer.c
@@ -50,6 +50,11 @@
 #define ACC_Z_RESP      0x32
 #define ACC_PITCH_RESP  0x33
 #define ACC_ROLL_RESP   0x34
+#define ACC_PITCH_RESP_NF  0x35
+#define ACC_ROLL_RESP_NF   0x36
+#define ACC_PITCH_RESP_SIMPLE 0x37
+#define ACC_ROLL_RESP_SIMPLE 0x38
+
 #define HIGH(x)  ((x) >> 8)
 #define LOW(x)  ((x) & 0xFF)
 /*****************************   Constants   *******************************/
@@ -59,8 +64,23 @@ extern FILE F_UART;
 INT16S acc_x_data = 0;
 INT16S acc_y_data = 0;
 INT16S acc_z_data = 0;
+INT8U acc_x0_data_raw = 0;
+INT16S acc_x_data_raw = 0;
+INT8U acc_y0_data_raw = 0;
+INT16S acc_y_data_raw = 0;
+INT8U acc_z0_data_raw = 0;
+INT16S acc_z_data_raw = 0;
 INT16S acc_pitch;
 INT16S acc_roll;
+INT16S acc_pitch_no_filter;
+INT16S acc_roll_no_filter;
+INT16S acc_pitch_simple_filter;
+INT16S acc_roll_simple_filter;
+
+INT16S acc_x_simple_filter = 0;
+INT16S acc_y_simple_filter = 0;
+INT16S acc_z_simple_filter = 0;
+
 volatile BOOLEAN acc_data_req = TRUE;
 extern BOOLEAN acc_test_on;
 
@@ -85,47 +105,73 @@ void init_accelerometer()
 }
 void update_acc_x0(INT8U ssi0_data)
 {
-    acc_x_data = ssi0_data;
+    acc_x0_data_raw = ssi0_data;
 }
 void update_acc_x1(INT8U ssi0_data)
 {
-    filter_x((ssi0_data<<8)|acc_x_data);
+    acc_x_data_raw = (ssi0_data<<8)|acc_x0_data_raw;
+    filter_x(acc_x_data_raw);
 }
 void update_acc_y0(INT8U ssi0_data)
 {
-    acc_y_data = ssi0_data;
+    acc_y0_data_raw = ssi0_data;
 }
 void update_acc_y1(INT8U ssi0_data)
 {
-    filter_y((ssi0_data<<8)|acc_y_data);
+    acc_y_data_raw = (ssi0_data<<8)|acc_y0_data_raw;
+    filter_y(acc_y_data_raw);
 }
 void update_acc_z0(INT8U ssi0_data)
 {
-    acc_z_data = ssi0_data;
+    acc_z0_data_raw = ssi0_data;
 }
 void update_acc_z1(INT8U ssi0_data)
 {
-    filter_z((ssi0_data<<8)|acc_z_data);
+    acc_z_data_raw = (ssi0_data<<8)|acc_z0_data_raw;
+    filter_z(acc_z_data_raw);
+
     acc_data_req = TRUE;
     acc_pitch = (atan2(acc_x_data*0.00390625,sqrt(acc_y_data*0.00390625*acc_y_data*0.00390625+acc_z_data*0.00390625*acc_z_data*0.00390625)) * 180.0) / PI;
     acc_roll = (atan2(acc_y_data*0.00390625,(sqrt(acc_x_data*0.00390625*acc_x_data*0.00390625+acc_z_data*0.00390625*acc_z_data*0.00390625))) * 180.0) / PI;
 
+    acc_pitch_no_filter = (atan2(acc_x_data_raw*0.00390625,sqrt(acc_y_data_raw*0.00390625*acc_y_data_raw*0.00390625+acc_z_data_raw*0.00390625*acc_z_data_raw*0.00390625)) * 180.0) / PI;
+    acc_roll_no_filter = (atan2(acc_y_data_raw*0.00390625,(sqrt(acc_x_data_raw*0.00390625*acc_x_data_raw*0.00390625+acc_z_data_raw*0.00390625*acc_z_data_raw*0.00390625))) * 180.0) / PI;
+
+    simple_filter_x(acc_x_data_raw);
+    simple_filter_y(acc_y_data_raw);
+    simple_filter_z(acc_z_data_raw);
+    acc_pitch_simple_filter = (atan2(acc_x_simple_filter*0.00390625,sqrt(acc_y_simple_filter*0.00390625*acc_y_simple_filter*0.00390625+acc_z_simple_filter*0.00390625*acc_z_simple_filter*0.00390625)) * 180.0) / PI;
+    acc_roll_simple_filter = (atan2(acc_y_simple_filter*0.00390625,(sqrt(acc_x_simple_filter*0.00390625*acc_x_simple_filter*0.00390625+acc_z_simple_filter*0.00390625*acc_z_simple_filter*0.00390625))) * 180.0) / PI;
+
+
     if(acc_test_on){
     file_write(F_UART, ACC_X_RESP);
-    file_write(F_UART, LOW(acc_x_data));
-    file_write(F_UART, HIGH(acc_x_data));
+    file_write(F_UART, LOW(acc_x_data_raw));
+    file_write(F_UART, HIGH(acc_x_data_raw));
     file_write(F_UART, ACC_Y_RESP);
-    file_write(F_UART, LOW(acc_y_data));
-    file_write(F_UART, HIGH(acc_y_data));
+    file_write(F_UART, LOW(acc_y_data_raw));
+    file_write(F_UART, HIGH(acc_y_data_raw));
     file_write(F_UART, ACC_Z_RESP);
-    file_write(F_UART, LOW(acc_z_data));
-    file_write(F_UART, HIGH(acc_z_data));
+    file_write(F_UART, LOW(acc_z_data_raw));
+    file_write(F_UART, HIGH(acc_z_data_raw));
     file_write(F_UART, ACC_PITCH_RESP);
     file_write(F_UART, LOW(acc_pitch));
     file_write(F_UART, HIGH(acc_pitch));
     file_write(F_UART, ACC_ROLL_RESP);
     file_write(F_UART, LOW(acc_roll));
     file_write(F_UART, HIGH(acc_roll));
+    file_write(F_UART, ACC_PITCH_RESP_NF);
+    file_write(F_UART, LOW(acc_pitch_no_filter));
+    file_write(F_UART, HIGH(acc_pitch_no_filter));
+    file_write(F_UART, ACC_ROLL_RESP_NF);
+    file_write(F_UART, LOW(acc_roll_no_filter));
+    file_write(F_UART, HIGH(acc_roll_no_filter));
+    file_write(F_UART, ACC_PITCH_RESP_SIMPLE);
+    file_write(F_UART, LOW(acc_pitch_simple_filter));
+    file_write(F_UART, HIGH(acc_pitch_simple_filter));
+    file_write(F_UART, ACC_ROLL_RESP_SIMPLE);
+    file_write(F_UART, LOW(acc_roll_simple_filter));
+    file_write(F_UART, HIGH(acc_roll_simple_filter));
     }
 }
 
@@ -229,3 +275,22 @@ void filter_z(INT16S next_input_value)
         acc_z_data = (INT16S)yv3[8];
 
   }
+
+void simple_filter_x(INT16S fXg)
+{
+    static FP64 xg = 0;
+    xg = (FP64)fXg * 0.05 + (xg * (1.0-0.05));
+    acc_x_simple_filter = (INT16S)xg;
+}
+void simple_filter_y(INT16S fYg)
+{
+    static FP64 yg = 0;
+    yg = (FP64)fYg * 0.05 + (yg * (1.0-0.05));
+    acc_y_simple_filter = (INT16S)yg;
+}
+void simple_filter_z(INT16S fZg)
+{
+    static FP64 zg = 0;
+    zg = (FP64)fZg * 0.05 + (zg * (1.0-0.05));
+    acc_z_simple_filter = (INT16S)zg;
+}
