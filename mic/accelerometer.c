874d4fd3c290fbc0354e3d7da8cb4852d8e7af9c
Mikkel Borring Jakobsen <mikkeljakobsen@hotmail.com>
2017-05-03 11:33:39 +0200

    Ready for master

--- mic/accelerometer.c
+++ mic/accelerometer.c
@@ -25,12 +25,8 @@
 #include "accelerometer.h"
 #include "rtcs.h"
 #include "ssi0.h"
-#include "file.h"
-#include "interval.h"
 #include "math.h"
 /*****************************    Defines    *******************************/
-#define PI 3.1415926535
-
 #define BW_RATE     0x2C
 #define POWER_CTL   0x2D
 #define INT_ENABLE  0x2E
@@ -42,36 +38,11 @@
 #define DATAY1      0x35
 #define DATAZ0      0x36
 #define DATAZ1      0x37
-
 #define READ_REG    (1<<7)
 #define DUMMY       0
-
-#define NZEROS 8
-#define NPOLES 8
-#define GAIN   8.881761746e+07
-
-#define ACC_X_RESP      0x30
-#define ACC_Y_RESP      0x31
-#define ACC_Z_RESP      0x32
-#define ACC_PITCH_RESP  0x33
-#define ACC_ROLL_RESP   0x34
-#define ACC_PITCH_RESP_NF  0x35
-#define ACC_ROLL_RESP_NF   0x36
-#define ACC_PITCH_RESP_SIMPLE 0x37
-#define ACC_ROLL_RESP_SIMPLE 0x38
-
-#define HIGH(x)  ((x) >> 8)
-#define LOW(x)  ((x) & 0xFF)
 /*****************************   Constants   *******************************/
-
-extern FILE F_UART;
 FP32 alpha = 0.1;
-
 /*****************************   Variables   *******************************/
-FP32 acc_x_xv[NZEROS+1], acc_x_yv[NPOLES+1];
-FP32 acc_y_xv[NZEROS+1], acc_y_yv[NPOLES+1];
-FP32 acc_z_xv[NZEROS+1], acc_z_yv[NPOLES+1];
-
 INT16S acc_x_data = 0;
 INT16S acc_y_data = 0;
 INT16S acc_z_data = 0;
@@ -81,17 +52,13 @@ INT8U acc_z_temp = 0;
 FP32 acc_x_filtered = 0;
 FP32 acc_y_filtered = 0;
 FP32 acc_z_filtered = 0;
-
 FP32 acc_pitch = 0;
 FP32 acc_roll = 0;
-
 FP32 acc_x_filtered_prev = 0;
 FP32 acc_y_filtered_prev = 0;
 FP32 acc_z_filtered_prev = 0;
 
-
-volatile BOOLEAN acc_data_req = TRUE;
-extern BOOLEAN acc_test_on;
+BOOLEAN acc_data_req = TRUE;
 /*****************************   Functions   *******************************/
 void init_accelerometer()
 {
@@ -135,28 +102,9 @@ void update_acc_z1(INT8U ssi0_data)
 {
     acc_z_data = (ssi0_data<<8)|acc_z_temp;
     acc_z_filtered = filter(acc_z_data, &acc_z_filtered_prev);
-
+    calc_pitch();
+    calc_roll();
     acc_data_req = TRUE;
-    acc_pitch = (atan2(acc_x_filtered*0.00390625,sqrt(acc_y_filtered*0.00390625*acc_y_filtered*0.00390625+acc_z_filtered*0.00390625*acc_z_filtered*0.00390625)) * 180.0) / PI;
-    acc_roll = (atan2(acc_y_filtered*0.00390625,(sqrt(acc_x_filtered*0.00390625*acc_x_filtered*0.00390625+acc_z_filtered*0.00390625*acc_z_filtered*0.00390625))) * 180.0) / PI;
-
-    if(acc_test_on){
-    file_write(F_UART, ACC_X_RESP);
-    file_write(F_UART, LOW(acc_x_data));
-    file_write(F_UART, HIGH(acc_x_data));
-    file_write(F_UART, ACC_Y_RESP);
-    file_write(F_UART, LOW(acc_y_data));
-    file_write(F_UART, HIGH(acc_y_data));
-    file_write(F_UART, ACC_Z_RESP);
-    file_write(F_UART, LOW(acc_z_data));
-    file_write(F_UART, HIGH(acc_z_data));
-    file_write(F_UART, ACC_PITCH_RESP);
-    file_write(F_UART, LOW((INT16S)acc_pitch));
-    file_write(F_UART, HIGH((INT16S)acc_pitch));
-    file_write(F_UART, ACC_ROLL_RESP);
-    file_write(F_UART, LOW((INT16S)acc_roll));
-    file_write(F_UART, HIGH((INT16S)acc_roll));
-    }
 }
 
 void accelerometer_task(INT8U my_id, INT8U my_state, INT8U event, INT8U data)
@@ -197,4 +145,23 @@ FP32 filter(FP32 acc_data, FP32 *acc_data_prev)
 {
     *acc_data_prev = acc_data * alpha + (*acc_data_prev * (1.0-alpha));
     return *acc_data_prev;
+}
+
+void calc_pitch()
+{
+    acc_pitch = atan2(acc_x_filtered*0.00390625,sqrt(acc_y_filtered*0.00390625*acc_y_filtered*0.00390625+acc_z_filtered*0.00390625*acc_z_filtered*0.00390625));
+}
+
+void calc_roll()
+{
+    acc_roll = atan2(acc_y_filtered*0.00390625,sqrt(acc_x_filtered*0.00390625*acc_x_filtered*0.00390625+acc_z_filtered*0.00390625*acc_z_filtered*0.00390625));
+}
+
+FP32 get_pitch()
+{
+    return acc_pitch;
+}
+FP32 get_roll()
+{
+    return acc_roll;
 }
--- mic/accelerometer.h
+++ mic/accelerometer.h
@@ -36,6 +36,10 @@ void update_acc_z0(INT8U ssi0_data);
 void update_acc_z1(INT8U ssi0_data);
 void accelerometer_task(INT8U my_id, INT8U my_state, INT8U event, INT8U data);
 FP32 filter(FP32 acc_data, FP32 *acc_data_prev);
+void calc_pitch();
+void calc_roll();
+FP32 get_pitch();
+FP32 get_roll();
 /*****************************************************************************
 *   Input    : -
 *   Output   : -
